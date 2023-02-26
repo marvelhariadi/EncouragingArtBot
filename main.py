d28083a65e382f6ecb30219538aa40aa72eccb1e
marvelhariadi <marvelhariadi@gmail.com>
2023-02-26 05:06:47 -0800

    added bob ross gifs functioanlity

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import discord
 import os
-from dotenv import load_dotenv #needed for os to work
+from dotenv import load_dotenv #needed for os to work. note: doesn't work for replit ver. as replit won't allow me to create .env file
 import requests
 import json
 import random
 from activewebserver import keep_alive
+import giphy_client
+
 
 load_dotenv() 
 intents = discord.Intents.default()
@@ -37,20 +39,21 @@ negSelfTalk = [
     "mistake"
 ]
 
-@client.event #when message is recieved
+@client.event #log-in conformation
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event #when bot makes message 
+@client.event #all bot outputs
 async def on_message(message):
     author = message.author.name
+    msg = message.content
 
     #guard
     if message.author == client.user: #if the author of the message comes from the bit
         return #do nothing
 
     #say hi
-    if message.content.lower().startswith('hi art bot'): #test
+    if msg.lower().startswith('hi art bot'): #test
         await message.channel.send(f'Hello, {author}! You certainly look friend-shaped today')
 
     #give encouragement
@@ -58,12 +61,32 @@ async def on_message(message):
         await message.channel.send(random.choice(cheerUppers))
 
     #vent function
-    if message.content.lower().startswith('venting: '):
-         await message.delete() #immediately deletes that function
-         await message.channel.send(f"thank you for sharing your hard feelings with me, {author}. I am proud of you <3")
+    if msg.lower().startswith('art bot, I need to vent') | msg.lower().startswith('art bot I need to vent'):
+        vent(author)
+
+    #bob ross gif function
+    if msg.lower().startswith('Bob Ross me'):
+        bobRossGif()
+
+    if any(word in msg == "sad"):
+        await message.channel.send("I'm sorry you've been down. Perhaps Papa Ross's wisdoms shall bring you peace")
+        bobRossGif()
+
+
+#abstracted functions
+@client.event
+async def vent(author):
+    await message.delete() #immediately deletes that function
+    await message.channel.send(f"thank you for sharing your hard feelings with me, {author}. I am proud of you <3")
+
+@client.event
+async def bobRossGif():
+    api_key = os.getenv('API_KEY') #replit issue
+    api_instance = giphy_client.DefaultApi()
+    api_response = api_instance.gifs_search_get(api_key, "bob+ross+quote", limit=5, rating='g')
 
 keep_alive()
-client.run(os.getenv('TOKEN')) #different on replit
+client.run(os.getenv('TOKEN')) #replit issue
  
 #bot features i want:
 #add a compliment
