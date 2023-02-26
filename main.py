2ff78cd1344047a18577672639dbc55a3d0c3604
marvelhariadi <marvelhariadi@gmail.com>
2023-02-26 05:39:42 -0800

    added bob ross gif functionalities ("bob ross me" command and "i'm sad" detection)

--- main.py
+++ main.py
@@ -1,94 +1,89 @@
 import discord
+from discord.ext import commands
 import os
-from dotenv import load_dotenv #needed for os to work. note: doesn't work for replit ver. as replit won't allow me to create .env file
+from dotenv import load_dotenv  #needed for os to work. note: doesn't work for replit ver. as replit won't allow me to create .env file
 import requests
 import json
 import random
 from activewebserver import keep_alive
 import giphy_client
 
-
-load_dotenv() 
+# load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+# client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 artEncouragements = [
-    "Your style is very beautiful",
-    "i see you've improved",
-    "Your art always brings me joy",
-    "I like your art style"
+  "Your style is very beautiful", "i see you've improved",
+  "Your art always brings me joy", "I like your art style"
 ]
 
 cheerUppers = [
-    "Hey!! Your art is freakin GORGEOUS and so are you",
-    "Believe in yourself or I will make you x", #threatening love
-    "The world would be so boring without your art",
-    "You should know everyone looks at your art with stars in their eyes",
-    f"Stop manifesting negativity in your life. You deserve to love yourself (and your art) the way I love you",
+  "Hey!! Your art is freakin GORGEOUS and so are you",
+  "Believe in yourself or I will make you x",  #threatening love
+  "The world would be so boring without your art",
+  "You should know everyone looks at your art with stars in their eyes",
+  f"Stop manifesting negativity in your life. You deserve to love yourself (and your art) the way I love you",
 ]
 
 negSelfTalk = [
-    "ugly",
-    "bad",
-    "I don't like my art style",
-    "I dont like my art style",
-    "horrendous",
-    "hate",
-    "mistake"
+  "ugly", "bad", "I don't like my art style", "I dont like my art style",
+  "horrendous", "hate", "mistake"
 ]
 
-@client.event #log-in conformation
+
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+  print('We have logged in as {0.user}'.format(bot))
 
-@client.event #all bot outputs
+
+@bot.event
 async def on_message(message):
-    author = message.author.name
-    msg = message.content
+  author = message.author.name
+  msg = message.content
+  ctx = await bot.get_context(message)
 
-    #guard
-    if message.author == client.user: #if the author of the message comes from the bit
-        return #do nothing
+  #guard
+  if message.author == bot.user:  #if the author of the message comes from the bit
+    return  #do nothing
 
-    #say hi
-    if msg.lower().startswith('hi art bot'): #test
-        await message.channel.send(f'Hello, {author}! You certainly look friend-shaped today')
+  #say hi
+  if msg.lower().startswith('hi art bot'):  #test
+    await message.channel.send(
+      f'Hello, {author}! You certainly look friend-shaped today')
 
-    #give encouragement
-    if any(word in msg for word in negSelfTalk):
-        await message.channel.send(random.choice(cheerUppers))
+  #give encouragement
+  if any(word in msg for word in negSelfTalk):
+    await message.channel.send(random.choice(cheerUppers))
 
-    #vent function
-    if msg.lower().startswith('art bot, I need to vent') | msg.lower().startswith('art bot I need to vent'):
-        vent(author)
+  #vent function
+  if msg.lower().startswith('vent time:'):
+    await message.channel.purge(1)  #immediately deletes that function
+    await message.channel.send(
+      f"thank you for sharing your hard feelings with me, {author}. I am proud of you <3"
+    )
 
-    #bob ross gif function
-    if msg.lower().startswith('Bob Ross me'):
-        bobRossGif()
+  #bob ross gif function
+  if msg.lower().startswith('bob ross me'):
+    await bobRossGif(ctx)
 
-    if any(word in msg == "sad"):
-        await message.channel.send("I'm sorry you've been down. Perhaps Papa Ross's wisdoms shall bring you peace")
-        bobRossGif()
+  if ("sad".lower() in msg):
+    await message.channel.send(
+      "I'm sorry you've been down. Perhaps Papa Ross's wisdoms shall bring you peace"
+    )
+    await bobRossGif(ctx)
 
 
-#abstracted functions
-@client.event
-async def vent(author):
-    await message.delete() #immediately deletes that function
-    await message.channel.send(f"thank you for sharing your hard feelings with me, {author}. I am proud of you <3")
+async def bobRossGif(ctx):
+  api_key = os.getenv("API_KEY")
+  api_instance = giphy_client.DefaultApi()
+  api_response = api_instance.gifs_search_get(api_key, "bob+ross+quote", limit=5, rating='g')
+  lst = list(api_response.data)
+  giffingtons = random.choice(lst)
+  await ctx.channel.send(giffingtons.embed_url)
 
-@client.event
-async def bobRossGif():
-    api_key = os.getenv('API_KEY') #replit issue
-    api_instance = giphy_client.DefaultApi()
-    api_response = api_instance.gifs_search_get(api_key, "bob+ross+quote", limit=5, rating='g')
 
 keep_alive()
-client.run(os.getenv('TOKEN')) #replit issue
- 
-#bot features i want:
-#add a compliment
-#say an art compliment
-#bonus: add a bob-ross gif. good trial for spn bot
+bot.run(os.getenv("TOKEN"))  #this line is different on github. des oke. replit doesn't like os
