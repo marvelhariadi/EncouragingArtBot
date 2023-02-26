4fc929ade75bb29bfe2c4de269bda1375ef60caf
marvelhariadi <marvelhariadi@gmail.com>
2023-02-25 23:00:00 -0800

    slay

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+
+client = discord.Client()
+
+@client.event #when message is recieved
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event #when bot makes message
+async def on_message():
+    if message.author == client.user: #if the author of the message comes from the bit
+        return #do nothing
+
+    if message.content.startswith()'$hello'):
+        await message.channel.send('Hello!')
+
+
+client.run()
